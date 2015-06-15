e1d42cd10bea8e5d88e45bad6698e9ece41c7bb7
steel <lordsteelhammer@gmail.com>
2015-06-15 11:58:15 +0200

    news redesign

--- news.php
+++ news.php
@@ -7,31 +7,27 @@ $query = "SELECT * FROM `hirek` ORDER BY id DESC LIMIT 25;";
 $result = mysql_query($query);
 
 while ($row = mysql_fetch_array($result)){
+    echo '<div class="col-md-12">';
     newsPrint($row["title"], $row["ndate"], $row["body"]);
+    echo '</div>';
 }
 
 function newsPrint($title, $date, $body){
-        echo "<div class='news'>
-                <div class='col-md-12'>
-                    <div id=''>
-                        <div class='panel panel-default'>
-                            <div class='panel-heading'>
-                                <div class='panel-title'>
-                                    <div class='row'>
-                                        <h4>
-                                            <div class='col-md-10'>".$title."</div>
-                                            <div class='col-md-2'>".$date."</div>
-                                        </h4>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class='panel-body'>
-                                    ".$body."
-                            </div>
+    echo "<div class='panel panel-default'>
+                <div class='panel-heading'>
+                    <div class='panel-title'>
+                        <div class='row'>
+                            <h4>
+                                <div class='col-md-10'>".$title."</div>
+                                <div class='col-md-2'>".$date."</div>
+                            </h4>
                         </div>
                     </div>
                 </div>
-            </div>";
+                <div class='panel-body'>
+                        ".$body."
+                </div>
+          </div>";
     }
 
 
